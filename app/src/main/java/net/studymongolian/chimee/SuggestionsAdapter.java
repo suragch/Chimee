689245330c2aaf76b6351f09d772bf991b39dd04
suragch <studymongolian@gmail.com>
2016-10-22 13:16:45 +0800

    suggestion bar words

--- app/src/main/java/net/studymongolian/chimee/SuggestionsAdapter.java
+++ app/src/main/java/net/studymongolian/chimee/SuggestionsAdapter.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -16,6 +17,8 @@ public class SuggestionsAdapter extends RecyclerView.Adapter<SuggestionsAdapter.
 
     private List<String> mData = Collections.emptyList();
     private LayoutInflater mInflater;
+    private ItemClickListener clickListener;
+
     //private Context mContext;
     MongolUnicodeRenderer renderer = MongolUnicodeRenderer.INSTANCE;
 
@@ -46,12 +49,37 @@ public class SuggestionsAdapter extends RecyclerView.Adapter<SuggestionsAdapter.
         return mData.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public void setClickListener(ItemClickListener itemClickListener) {
+        this.clickListener = itemClickListener;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public TextView tvWord;
 
         public ViewHolder(View itemView) {
             super(itemView);
             tvWord = (TextView) itemView.findViewById(R.id.tvRecyclerViewSuggestedWord);
+            itemView.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View view) {
+            if (clickListener != null) clickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+    public void swap(List<String> newList){
+        mData.clear();
+        mData.addAll(newList);
+        notifyDataSetChanged();
+    }
+
+    public void clear(){
+        mData.clear();
+        notifyDataSetChanged();
+    }
+
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
     }
 }
