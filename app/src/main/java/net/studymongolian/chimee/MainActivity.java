c3629ac6597424f8bf32c8833b098fbdbf93a703
suragch <studymongolian@gmail.com>
2018-04-23 20:20:57 +0800

    input window resizing up working

--- app/src/main/java/net/studymongolian/chimee/MainActivity.java
+++ app/src/main/java/net/studymongolian/chimee/MainActivity.java
@@ -15,6 +15,7 @@ import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -24,90 +25,90 @@ import android.widget.HorizontalScrollView;
 
 public class MainActivity extends AppCompatActivity {
 
-	protected static final int SHARE_CHOOSER_REQUEST = 0;
-	protected static final int WECHAT_REQUEST = 1;
-	protected static final int SETTINGS_REQUEST = 2;
-	protected static final int FAVORITE_MESSAGE_REQUEST = 3;
-	protected static final int HISTORY_REQUEST = 4;
+    protected static final int SHARE_CHOOSER_REQUEST = 0;
+    protected static final int WECHAT_REQUEST = 1;
+    protected static final int SETTINGS_REQUEST = 2;
+    protected static final int FAVORITE_MESSAGE_REQUEST = 3;
+    protected static final int HISTORY_REQUEST = 4;
     protected static final int PHOTO_OVERLAY_REQUEST = 6;
 
-	private enum ShareType {
-		WeChat,
-		Bainu,
-		Other
-	}
+    private enum ShareType {
+        WeChat,
+        Bainu,
+        Other
+    }
 
-	ResizingScrollView inputWindow;
-	HorizontalScrollView hsvScrollView;
-	FrameLayout rlTop;
-	Dialog overflowMenu;
-	Dialog contextMenu;
-	static final int INPUT_WINDOW_SIZE_INCREMENT_DP = 50;
-	int inputWindowSizeIncrementPx = 0;
-	static final int INPUT_WINDOW_MIN_HEIGHT_DP = 150;
-	int inputWindowMinHeightPx = 0;
-	SharedPreferences settings;
-	String lastSentMessage = ""; // don't save two same messages to history
+    ResizingScrollView inputWindow;
+    HorizontalScrollView hsvScrollView;
+    FrameLayout rlTop;
+    Dialog overflowMenu;
+    Dialog contextMenu;
+    static final int INPUT_WINDOW_SIZE_INCREMENT_DP = 50;
+    int inputWindowSizeIncrementPx = 0;
+    static final int INPUT_WINDOW_MIN_HEIGHT_DP = 150;
+    int inputWindowMinHeightPx = 0;
+    SharedPreferences settings;
+    String lastSentMessage = ""; // don't save two same messages to history
 
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		// setup toolbar
-		Toolbar myToolbar = findViewById(R.id.my_toolbar);
-		setSupportActionBar(myToolbar);
+        // setup toolbar
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
-		// disable rotation for smaller devices
-		if(getResources().getBoolean(R.bool.portrait_only)){
-			setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-		}
+        // disable rotation for smaller devices
+        if (getResources().getBoolean(R.bool.portrait_only)) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
 
-		// set up the keyboard
-		ImeContainer imeContainer = findViewById(R.id.imeContainer);
-		inputWindow = findViewById(R.id.resizingScrollView);
-		MongolEditText editText = inputWindow.getEditText();
-		MongolInputMethodManager mimm = new MongolInputMethodManager();
-		mimm.addEditor(editText);
-		mimm.setIme(imeContainer);
+        // set up the keyboard
+        ImeContainer imeContainer = findViewById(R.id.imeContainer);
+        inputWindow = findViewById(R.id.resizingScrollView);
+        MongolEditText editText = inputWindow.getEditText();
+        MongolInputMethodManager mimm = new MongolInputMethodManager();
+        mimm.addEditor(editText);
+        mimm.setIme(imeContainer);
 
-		settings = getSharedPreferences(SettingsActivity.PREFS_NAME, MODE_PRIVATE);
-		// TODO get the right keyboard
+        settings = getSharedPreferences(SettingsActivity.PREFS_NAME, MODE_PRIVATE);
+        // TODO get the right keyboard
         // TODO get a saved draft
 
 
-		// TODO get input window and set listeners
+        // TODO get input window and set listeners
         //hsvScrollView = findViewById(R.id.horizontalScrollView);
-		//initInputWindow();
+        //initInputWindow();
 
         showWeChatButton();
 
 
-		// Set up density independent pixel constants
-		final float scale = getResources().getDisplayMetrics().density;
-		inputWindowSizeIncrementPx = (int) (INPUT_WINDOW_SIZE_INCREMENT_DP * scale + 0.5f);
-		inputWindowMinHeightPx = (int) (INPUT_WINDOW_MIN_HEIGHT_DP * scale + 0.5f);
+        // Set up density independent pixel constants
+        final float scale = getResources().getDisplayMetrics().density;
+        inputWindowSizeIncrementPx = (int) (INPUT_WINDOW_SIZE_INCREMENT_DP * scale + 0.5f);
+        inputWindowMinHeightPx = (int) (INPUT_WINDOW_MIN_HEIGHT_DP * scale + 0.5f);
 
 
-	}
+    }
 
     private void showWeChatButton() {
         // If WeChat is installed make the button visible
-		String weChatMessageTool = "com.tencent.mm.ui.tools.ShareImgUI";
-		Intent shareIntent = new Intent();
-		shareIntent.setType("image/png");
-		List<ResolveInfo> resInfo = getPackageManager().queryIntentActivities(shareIntent, 0);
-		if (!resInfo.isEmpty()) {
-			for (ResolveInfo info : resInfo) {
-				if (info.activityInfo.name.equals(weChatMessageTool)) {
-				    // FIXME
-					//FrameLayout weChatButton = (FrameLayout) findViewById(R.id.shareToWeChatFrame);
-					//weChatButton.setVisibility(View.VISIBLE);
-					break;
-				}
-			}
-		}
+        String weChatMessageTool = "com.tencent.mm.ui.tools.ShareImgUI";
+        Intent shareIntent = new Intent();
+        shareIntent.setType("image/png");
+        List<ResolveInfo> resInfo = getPackageManager().queryIntentActivities(shareIntent, 0);
+        if (!resInfo.isEmpty()) {
+            for (ResolveInfo info : resInfo) {
+                if (info.activityInfo.name.equals(weChatMessageTool)) {
+                    // FIXME
+                    //FrameLayout weChatButton = (FrameLayout) findViewById(R.id.shareToWeChatFrame);
+                    //weChatButton.setVisibility(View.VISIBLE);
+                    break;
+                }
+            }
+        }
     }
 
 
@@ -115,21 +116,32 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
 
-		// If WeChat is installed make the menu item visible
-		String weChatMessageTool = "com.tencent.mm.ui.tools.ShareImgUI";
-		Intent shareIntent = new Intent();
-		shareIntent.setType("image/png");
-		List<ResolveInfo> resInfo = getPackageManager().queryIntentActivities(shareIntent, 0);
-		if (!resInfo.isEmpty()) {
-			for (ResolveInfo info : resInfo) {
-				if (info.activityInfo.name.equals(weChatMessageTool)) {
-					MenuItem item = menu.findItem(R.id.main_action_wechat);
-					item.setVisible(true);
-					break;
-				}
-			}
-		}
+        // If WeChat is installed make the menu item visible
+        String weChatMessageTool = "com.tencent.mm.ui.tools.ShareImgUI";
+        Intent shareIntent = new Intent();
+        shareIntent.setType("image/png");
+        List<ResolveInfo> resInfo = getPackageManager().queryIntentActivities(shareIntent, 0);
+        if (!resInfo.isEmpty()) {
+            for (ResolveInfo info : resInfo) {
+                if (info.activityInfo.name.equals(weChatMessageTool)) {
+                    MenuItem item = menu.findItem(R.id.main_action_wechat);
+                    item.setVisible(true);
+                    break;
+                }
+            }
+        }
 
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        FrameLayout topFrame = findViewById(R.id.rlTop);
+        ResizingScrollView inputWindow = findViewById(R.id.resizingScrollView);
+
+        Log.i("TAG", "onOptionsItemSelected: "
+                + topFrame.getHeight() + " "
+                + inputWindow.getHeight());
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/net/studymongolian/chimee/ResizingScrollView.java
+++ app/src/main/java/net/studymongolian/chimee/ResizingScrollView.java
@@ -19,6 +19,7 @@ public class ResizingScrollView extends HorizontalScrollView {
     private float mOldWidth = 0;
     private int mDesiredHeight;
     private int mDesiredWidth;
+    private int mHeightStepPx;
 
 
     private MongolEditText editText;
@@ -41,6 +42,7 @@ public class ResizingScrollView extends HorizontalScrollView {
     private void init(Context context, AttributeSet attrs, int defStyleAttr) {
         mDesiredHeight = (int) convertDpToPx(MIN_HEIGHT_DP);
         mDesiredWidth = (int) convertDpToPx(MIN_WIDTH_DP);
+        mHeightStepPx = (int) convertDpToPx(HEIGHT_STEP_DP);
 
         editText = new MongolEditText(context, attrs, defStyleAttr);
         editText.setPadding(10, 10, 10, 10);
@@ -55,6 +57,10 @@ public class ResizingScrollView extends HorizontalScrollView {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 
         final int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        //int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
         if (widthMode == MeasureSpec.UNSPECIFIED) {
             //return;
         }
@@ -65,15 +71,24 @@ public class ResizingScrollView extends HorizontalScrollView {
         editText.measure(specWidth, specHeight);
 
         int editTextWidth = editText.getMeasuredWidth();
+        int editTextHeight = editText.getMeasuredHeight();
         if (editTextWidth > mDesiredWidth) {
             mDesiredWidth = editTextWidth;
+            if (mDesiredWidth > mDesiredHeight) {
+                int newHeight = (int) Math.sqrt(2*editTextHeight*editTextWidth);
+                if (newHeight - mDesiredHeight > mHeightStepPx) {
+                    mDesiredHeight += mHeightStepPx;
+                }
+                if (mDesiredHeight > heightSize) {
+                    mDesiredHeight = Math.min(mDesiredHeight, heightSize);
+                }
+                specHeight = MeasureSpec.makeMeasureSpec(mDesiredHeight, MeasureSpec.EXACTLY);
+                editText.measure(specWidth, specHeight);
+                mDesiredWidth = editText.getMeasuredWidth();
+            }
         }
         Log.i("TAG", "onMeasure: " + mDesiredWidth);
 
-        //int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
-        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
 
         int width;
         int height;
@@ -94,6 +109,11 @@ public class ResizingScrollView extends HorizontalScrollView {
             height = Math.min(mDesiredHeight, heightSize);
         } else {
             height = mDesiredHeight;
+        }
+
+        if (height != mDesiredHeight) {
+            specHeight = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+            editText.measure(specWidth, specHeight);
         }
 
         setMeasuredDimension(width, height);
